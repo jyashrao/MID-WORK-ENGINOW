3ea98b7072fa5a2ca3a0e16bf82223671cb3b6ca
1010YASH <jyashrao10@gmail.com>
2026-06-07 00:04:56 +0530

    base added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MID-WORK-ENGINOW/',
   define: {
     // This allows some libraries to access process.env.NODE_ENV
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
