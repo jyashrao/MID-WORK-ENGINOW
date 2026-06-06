e3d4c7a5bcb8992ef204a330ca2d804fc1f53502
1010YASH <jyashrao10@gmail.com>
2026-06-07 00:23:44 +0530

    vercel fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MID-WORK-ENGINOW/',
   define: {
     // This allows some libraries to access process.env.NODE_ENV
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
